a3d380f8161ac72e6b22d6f7336d69ffab964448
vivaHYSA <vivacious@kakao.com>
2022-06-23 14:52:13 +0900

    add method and string ...

--- account/account.go
+++ account/account.go
@@ -1,14 +1,57 @@
 package account
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Account struct
 type Account struct{
 	owner string
 	balance int
 }
 
+var errNoMoney = errors.New("there is no money");
+
+// NewAccount creates Account
 func NewAccount(owner string) *Account{
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount;
+	
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// WithDraw x amount from your account
+func (a *Account) WithDraw(amount int) error {
+	if a.balance < amount{
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of your account
+func (a Account) Owner() string{
+	return a.owner
+}
+
+func (a Account) String() string{
+	return fmt.Sprint("test for ", a.owner ," is balance : ", a.balance);
 }
 
 
--- main.go
+++ main.go
@@ -12,6 +12,5 @@ func main(){
 	result := account.NewAccount("jiwan");
 	
 	fmt.Println(result);
-	
 
 }
